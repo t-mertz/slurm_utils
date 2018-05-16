d1fc26ceb69894a4ff5e0573ef736e278cf28a11
Thomas Mertz <mertz@users.itp.uni-frankfurt.de>
2018-05-16 20:16:09 +0200

    SLURM configuration
    * New subpackage slurm_interface
    * Added new data types in slurm_interface.config

---
+++ sutils/slurm_interface/tests/config.py
@@ -0,0 +1,8 @@
+import unitest
+from .. import config
+
+
+class TestConfig(unittest.TestCase):
+    def test_time_full(self):
+        time = config.SBATCH_time("1-00:01:1")
+        self.assertEqual(time.to_str(), "1-00:01:01")
