6e0751288262a41030205a6e373b5dd61ee025ac
Thomas Mertz <t-mertz@users.noreply.github.com>
2018-06-05 13:16:35 +0200

    Bugfix
    processing now works as expected.

--- sutils/processing/fargs.py
+++ sutils/processing/fargs.py
@@ -2,4 +2,4 @@
 
 def get_number_fargs(func):
     """Return the number of function arguments"""
-    return len(func.func_code.co_varnames)
+    return func.func_code.co_argcount
--- sutils/processing/process.py
+++ sutils/processing/process.py
@@ -1,24 +1,25 @@
 # This file is part of the SLURM utility
 # Implements a user interface for the data processor
 import subprocess
+import sys
 
 from ..core import core
 import fargs
 
-def process(func):
+def process_py(func):
     try:
-        proc = get_processor(func)
+        proc = get_processor("python", func)
     except ValueError as e:
-        sys.stdout.write("An error occurred: " + str(e))
+        sys.stdout.write("An error occurred: " + str(e) + "\n")
         sys.exit(1)
 
     proc.run()
 
 
-class DataProcessor(core.ParameterIterator):
+class DataProcessor(core.DataProcessor):
     """Run specified Python function on data."""
     def __init__(self, func):
-        super().__init__(self)
+        super(DataProcessor, self).__init__()
         #check if func takes three arguments!
         if fargs.get_number_fargs(func) == 3:
             self.process = func
@@ -28,13 +29,17 @@ class DataProcessor(core.ParameterIterator):
     def run(self):
         self.iterate()
 
-def get_processor(func):
-    return DataProcessor(func)
+def get_processor(script_type, func):
+    """Factory function for processors"""
+    if script_type.lower() == 'python':
+        return DataProcessor(func)
+    else:
+        raise NotImplementedError(script_type)
 
 class ShellProcessor(core.ParameterIterator):
     """Run shell script on data."""
     def __init__(self, script_name):
-        super().__init__(self)
+        super(ShellProcessor, self).__init__()
         self._script_name = script_name
 
     def process(self, dirname, job_idx, plist):
