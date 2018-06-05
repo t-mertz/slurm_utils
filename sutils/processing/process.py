cbe7ff0f7b7510ff81964a924733732cadd263fb
Thomas Mertz <t-mertz@users.noreply.github.com>
2018-06-05 10:39:46 +0200

    Bugfix and simplified processing
    * new more helpful info message when number of jobs to submit is out of
      range
    * new processing.fargs.get_number_fargs to query the number of function
      arguments
    * processing.process.process now performs a sanity check on the number
      of function arguments (must be 3)

--- sutils/processing/process.py
+++ sutils/processing/process.py
@@ -3,9 +3,16 @@
 import subprocess
 
 from ..core import core
+import fargs
 
 def process(func):
-    get_processor(func).run()
+    try:
+        proc = get_processor(func)
+    except ValueError as e:
+        sys.stdout.write("An error occurred: " + str(e))
+        sys.exit(1)
+
+    proc.run()
 
 
 class DataProcessor(core.ParameterIterator):
@@ -13,7 +20,10 @@ class DataProcessor(core.ParameterIterator):
     def __init__(self, func):
         super().__init__(self)
         #check if func takes three arguments!
-        self.process = func
+        if fargs.get_number_fargs(func) == 3:
+            self.process = func
+        else:
+            raise ValueError("Function must take exactly three arguments (dirname, job_idx, plist).")
         
     def run(self):
         self.iterate()
