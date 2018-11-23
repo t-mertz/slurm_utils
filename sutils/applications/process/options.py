581ed06a64e169f3f50d31f9e19f3fc066d8976d
Thomas Mertz <17323028+t-mertz@users.noreply.github.com>
2018-11-23 14:48:58 +0100

    Added new command line options for process application

--- sutils/applications/process/options.py
+++ sutils/applications/process/options.py
@@ -1,4 +1,7 @@
 from ...utils.argparse_wrap import ArgumentParser
 
 parser = ArgumentParser(description="Run processing executable in every directory.") \
-            .add_argument('filename', nargs=1, type=str, help="name of or path to the file")
+            .add_argument('filename', metavar='FILE', nargs=1, type=str, help="name of or path to the file") \
+            .add_argument(['-r', '--root_path'], metavar='ROOTPATH', nargs=1, type=str, help="root directory of the calculation") \
+            .add_argument(['-c', '--configfile'], metavar='FILENAME', nargs=1, type=str, help="name of the config file") \
+            .add_argument('start', metavar='START', nargs='?', type=int, help="first job to consider")
