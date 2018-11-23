0d235a89b9cb9f0603053169de63432a4872c3b1
Thomas Mertz <17323028+t-mertz@users.noreply.github.com>
2018-11-23 15:07:33 +0100

    Fixed commandline arguments for process app

--- sutils/applications/process/options.py
+++ sutils/applications/process/options.py
@@ -1,7 +1,7 @@
 from ...utils.argparse_wrap import ArgumentParser
 
 parser = ArgumentParser(description="Run processing executable in every directory.") \
-            .add_argument('filename', metavar='FILE', nargs=1, type=str, help="name of or path to the file") \
-            .add_argument(['-r', '--root_path'], metavar='ROOTPATH', nargs=1, type=str, help="root directory of the calculation") \
-            .add_argument(['-c', '--configfile'], metavar='FILENAME', nargs=1, type=str, help="name of the config file") \
+            .add_argument('filename', metavar='FILE', type=str, help="name of or path to the file") \
+            .add_argument('-d', '--directory', metavar='PATH', type=str, help="root directory of the calculation") \
+            .add_argument('-f', '--file', metavar='FILENAME', type=str, help="name of the config file") \
             .add_argument('start', metavar='START', nargs='?', type=int, help="first job to consider")
